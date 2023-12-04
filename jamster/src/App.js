ca0f77eca16e2e2fb9978248d57b2e306cdb2247
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-12-04 14:20:42 -0800

    Added link to extension gh

--- jamster/src/App.js
+++ jamster/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Song from "./Components/song.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Song></Song>
-    </div>
-  );
+    return (
+        <div className="App">
+            <a href="https://github.com/seika5/jamster">Browser Extension</a>
+            <Song></Song>
+        </div>
+    );
 }
 
 export default App;
