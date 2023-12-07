1ab6e2a9a0bfe7f9565fb5e6043a7e28d41f33fe
Ryan Mar <69983436+seika5@users.noreply.github.com>
2023-12-07 15:18:00 -0800

    Added title

--- jamster/src/App.js
+++ jamster/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Song from "./Components/song.js";
 
 function App() {
+    document.title = "Jamster";
     return (
         <div className="App">
             <a href="https://github.com/seika5/jamster">Browser Extension</a>
